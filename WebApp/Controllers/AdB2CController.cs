3ae2eef19514c166d7a4497f154ca579a61ee645
Nikolaos Antoniou <niantoni@microsoft.com>
2018-03-13 10:25:01 +0200

    Add project files.

---
+++ WebApp/Controllers/AdB2CController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Extensions;
+
+namespace WebApp.Controllers
+{
+
+    /// <summary>
+    /// This controller delivers the custom UI for the needs of AD B2C. The custom UI pages can be simple static Html pages,
+    /// or have server logic during the generation of the wrapping content. 
+    /// NOTE: The server hosting the custom UI should allow CORS requests on the resources required.
+    /// </summary>
+    [AllowCors]
+    public class AdB2CController : Controller
+    {
+        public ActionResult SignInSignUp()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SocialSignUp(string campaign)
+        {
+            return View((object)campaign);
+        }
+
+
+        public ActionResult IdpSelector()
+        {
+            return View();
+        }
+
+        public ActionResult MultiFactorAuthentication()
+        {
+            return View();
+        }
+
+        public ActionResult Unified(string campaign)
+        {
+            return View((object)campaign);
+        }
+
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateProfile()
+        {
+            return View();
+        }
+    }
+}
