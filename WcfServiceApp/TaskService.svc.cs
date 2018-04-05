e4b27f5bd297c9d35240ddfa978797195eb6dab9
Nikolaos Antoniou <niantoni@microsoft.com>
2018-04-05 12:56:41 +0300

    Tidying up

--- WcfServiceApp/TaskService.svc.cs
+++ WcfServiceApp/TaskService.svc.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
-using System.Security.Permissions;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 using System.Threading;
 using WcfServiceApp.Models;
 
 namespace WcfServiceApp
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-
-    //[PrincipalPermission(SecurityAction.Demand)]
     public class TaskService : ITaskService
     {
         private static readonly ITodoRepository repository = new TodoInMemoryRepository();
