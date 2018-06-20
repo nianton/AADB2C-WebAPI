5edd8fa6406a6eca7e77a2887983d5e4b42552a3
Nikolaos Antoniou <nianton@gmail.com>
2018-06-20 13:55:04 +0300

    Custom policies / azure function integration

--- MigrationFuncApp/SignIn.cs
+++ MigrationFuncApp/SignIn.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -19,12 +20,19 @@ namespace MigrationFuncApp
             TraceWriter log)
         {
             log.Info("SignIn HTTP trigger function processed a request.");
-            var model = await req.Content.ReadAsAsync<SignInModel>();
+
+            var ctx = (HttpContextWrapper)req.Properties["MS_HttpContext"];
+            var callerAddress = ctx.Request.UserHostAddress;
+            log.Info($"Received request from: {callerAddress}");
+            
+            var model = await req.Content.ReadAsAsync<SignInModel>();            
+            log.Info($"SigninName: {model?.SignInName}, Password: {model?.Password}");
+
             var name = model?.SignInName;
 
             return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, ErrorResponseModel.Create("Please pass a name in the request body", HttpStatusCode.BadRequest))
+                ? req.CreateResponse(HttpStatusCode.BadRequest, ErrorResponseModel.Create("Please pass the 'signInName' claim in the request body", HttpStatusCode.BadRequest))
                 : req.CreateResponse(HttpStatusCode.OK);
-        }        
+        }
     }
 }
